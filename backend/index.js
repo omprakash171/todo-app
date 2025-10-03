67153dede259715af1180c840690fd8b15c88337
Om Prakash <bomprakash485@gmail.com>
2025-10-03 13:44:27 +0530

    added delete functionality

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import e from "express";
 import { collectionName, connection } from "./dbconfig.js";
 import cors from "cors"
+import { ObjectId } from "mongodb";
 const app = e();
 
 app.use(e.json());
@@ -22,6 +23,18 @@ app.get("/tasks", async (req, resp) => {
     const result = await collection.find().toArray();
     if(result){
         resp.send({message: "task list fetched", success: true, result})
+    } else {
+        resp.send({message: "error try after sometime", success: false})
+    }
+})
+
+app.delete("/delete/:id", async (req, resp) => {
+    const db = await connection();
+    const id = req.params.id
+    const collection = await db.collection(collectionName);
+    const result = await collection.deleteOne({_id: new ObjectId(id)})
+    if(result){
+        resp.send({message: "task deleted", success: true, result})
     } else {
         resp.send({message: "error try after sometime", success: false})
     }
